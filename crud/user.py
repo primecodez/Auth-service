adb2a20aca97b2220aca67375dd7dbdd735eeae8
Narendra Singh <narendrakumarsingh2859@gmail.com>
2026-08-10 14:46:22 +0000

    Add user id

--- crud/user.py
+++ crud/user.py
@@ -20,3 +20,10 @@ def create_user(
     db.refresh(user)
 
     return user
+
+
+def get_user_by_id(
+    db: Session,
+    user_id: int,
+) -> User | None:
+    return db.query(User).filter(User.id == user_id).first()
